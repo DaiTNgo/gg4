f29ad283151d2f1d3b3b361f487264488d4ab188
daingo <daingo7211@gmail.com>
2022-11-14 11:48:20 +0700

    4

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {BrowserRouter, createBrowserRouter, Route, Link, RouterProvider, Routes} from "react-router-dom";
+import Contact from "./Contact";
+import About from "./About";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/contact'}>Contact</Link>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="about" element={<About/>}/>
+                <Route path="contact" element={<Contact/>}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 )
