f7c25e8f855b7fe49dbfd5a71f2403986c58d608
daingo <daingo7211@gmail.com>
2022-11-14 13:40:03 +0700

    event user

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReactGA from 'react-ga';
@@ -10,40 +10,46 @@ ReactGA.pageview('/contact');
 
 function App() {
     const [count, setCount] = useState(0);
-
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+        ReactGA.event({
+            category: 'User',
+            action: 'Clicked Button'
+        })
+    }, []);
     return (<React.Fragment>
-            <div className='App'>
-                <header className='App-header'>
-                    <img src={logo} className='App-logo' alt='logo'/>
-                    <p>Hello Vite + React!</p>
-                    <p>
-                        <button type='button' onClick={() => setCount((count) => count + 1)}>
-                            count is: {count}
-                        </button>
-                    </p>
-                    <p>
-                        Edit <code>App.jsx</code> and save to test HMR updates.
-                    </p>
-                    <p>
-                        <a
-                            className='App-link'
-                            href='https://reactjs.org'
-                            target='_blank'
-                            rel='noopener noreferrer'>
-                            Learn React
-                        </a>
-                        {' | '}
-                        <a
-                            className='App-link'
-                            href='https://vitejs.dev/guide/features.html'
-                            target='_blank'
-                            rel='noopener noreferrer'>
-                            Vite Docs
-                        </a>
-                    </p>
-                </header>
-            </div>
-        </React.Fragment>);
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo'/>
+                <p>Hello Vite + React!</p>
+                <p>
+                    <button type='button' onClick={() => setCount((count) => count + 1)}>
+                        count is: {count}
+                    </button>
+                </p>
+                <p>
+                    Edit <code>App.jsx</code> and save to test HMR updates.
+                </p>
+                <p>
+                    <a
+                        className='App-link'
+                        href='https://reactjs.org'
+                        target='_blank'
+                        rel='noopener noreferrer'>
+                        Learn React
+                    </a>
+                    {' | '}
+                    <a
+                        className='App-link'
+                        href='https://vitejs.dev/guide/features.html'
+                        target='_blank'
+                        rel='noopener noreferrer'>
+                        Vite Docs
+                    </a>
+                </p>
+            </header>
+        </div>
+    </React.Fragment>);
 }
 
 export default App;
