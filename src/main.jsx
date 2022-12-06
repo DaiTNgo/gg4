e4fc9416cd45c1a13b1918772ef971d003302b21
daingo <daingo7211@gmail.com>
2022-12-06 16:35:24 +0700

    add gtm

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import {BrowserRouter, createBrowserRouter, Route, Link, RouterProvider, Routes} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import {
+    BrowserRouter,
+    createBrowserRouter,
+    Route,
+    Link,
+    RouterProvider,
+    Routes,
+} from "react-router-dom";
 import Contact from "./Contact";
 import About from "./About";
 import ReactGA from "react-ga4";
 import Info from "./Info";
 
-ReactGA.initialize('G-BW68E5EZ6R');
+ReactGA.initialize("G-BW68E5EZ6R");
+window.dataLayer = window.dataLayer || [];
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-BW68E5EZ6R");
+
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/about'}>About</Link>
-            <Link to={'/contact'}>Contact</Link>
-            <Link to={'/info'}>Info</Link>
+            <Link to={"/"}>Home</Link>
+            <Link to={"/about"}>About</Link>
+            <Link to={"/contact"}>Contact</Link>
+            <Link to={"/info"}>Info</Link>
             <Routes>
-                <Route path="/" element={<App/>}/>
-                <Route path="about" element={<About/>}/>
-                <Route path="contact" element={<Contact/>}/>
-                <Route path="info" element={<Info/>}/>
+                <Route path="/" element={<App />} />
+                <Route path="about" element={<About />} />
+                <Route path="contact" element={<Contact />} />
+                <Route path="info" element={<Info />} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
-)
+);
