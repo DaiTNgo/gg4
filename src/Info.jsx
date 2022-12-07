22016cf6dfb640629aeea8ae2aba6e613ac9f1df
daingo <daingo7211@gmail.com>
2022-12-07 11:12:09 +0700

    add info

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,37 +1,37 @@
-import React, { useEffect, useRef, useState } from "react";
-// import ReactGA from "react-ga4";
+import React, { useEffect, useRef, useState } from 'react';
+import ReactGA from 'react-ga4';
 
 function Info() {
-    const [text, setText] = useState("");
-    const refText = useRef();
-    // useEffect(() => {
-    //     ReactGA.send({
-    //         hitType: 'pageview',
-    //         page: '/infodaingo'
-    //     })
-    // }, [])
+  const [text, setText] = useState('');
+  const refText = useRef();
+  useEffect(() => {
+    ReactGA.send({
+      hitType: 'pageview',
+      page: '/infodaingo',
+    });
+  }, []);
 
-    const handleSendEvent = () => {
-        // ReactGA.event({
-        //     category: 'Textttttttttt',
-        //     action: 'Text: ' + text
-        // })
+  const handleSendEvent = () => {
+    ReactGA.event({
+      category: 'Textttttttttt',
+      action: 'Text: ' + text,
+    });
 
-        setText("");
-        refText.current.focus();
-    };
-    return (
-        <div>
-            <h1>{text}</h1>
-            <input
-                ref={refText}
-                type="text"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-            />
-            <button onClick={handleSendEvent}>Send Event</button>
-        </div>
-    );
+    setText('');
+    refText.current.focus();
+  };
+  return (
+    <div>
+      <h1>{text}</h1>
+      <input
+        ref={refText}
+        type='text'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={handleSendEvent}>Send Event</button>
+    </div>
+  );
 }
 
 export default Info;
