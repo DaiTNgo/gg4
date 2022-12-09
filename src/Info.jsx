fd8810e9fd999101b49125427191a94226c51873
daingo <daingo7211@gmail.com>
2022-12-09 15:03:35 +0700

    gtm button

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,40 +1,49 @@
-import React, { useEffect, useRef, useState } from 'react';
-import ReactGA from 'react-ga4';
+import React, { useEffect, useRef, useState } from "react";
+import ReactGA from "react-ga4";
 
 function Info() {
-  const [text, setText] = useState('');
-  const refText = useRef();
-  useEffect(() => {
-    ReactGA.send({
-      hitType: 'pageview',
-      page: '/infodaingo',
-    });
-  }, []);
+    const [text, setText] = useState("");
+    const refText = useRef();
+    useEffect(() => {
+        ReactGA.send({
+            hitType: "pageview",
+            page: "/infodaingo",
+        });
+    }, []);
 
-  const handleSendEvent = () => {
-    // ReactGA.event({
-    //   category: 'Textttttttttt',
-    //   action: 'Text: ' + text,
-    // });
-    window.dataLayer.push({
-      category: 'Textttttttttt',
-      action: 'Text: ' + text,
-    });
-    setText('');
-    refText.current.focus();
-  };
-  return (
-    <div>
-      <h1>{text}</h1>
-      <input
-        ref={refText}
-        type='text'
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={handleSendEvent}>Send Event</button>
-    </div>
-  );
+    const handleSendEvent = () => {
+        // ReactGA.event({
+        //   category: 'Textttttttttt',
+        //   action: 'Text: ' + text,
+        // });
+        window.dataLayer.push({
+            category: "Textttttttttt",
+            action: "Text: " + text,
+        });
+        setText("");
+        refText.current.focus();
+    };
+    return (
+        <div>
+            <h1>{text}</h1>
+            <input
+                ref={refText}
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+            <button onClick={handleSendEvent}>Send Event</button>
+            <br />
+            <button id="btn-1" className="button-1">
+                Button 1
+            </button>
+
+            <br />
+            <button id="btn-2" className="button-2">
+                Button 2
+            </button>
+        </div>
+    );
 }
 
 export default Info;
